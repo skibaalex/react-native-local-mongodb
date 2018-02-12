b97200f0d8e5098b790e47bb67d2444ad3f28d89
antoniosilva <antoniopresto@gmail.com>
2018-02-12 21:50:08 -0200

    promisefy

--- test/persistence.test.js
+++ test/persistence.test.js
@@ -6,6 +6,15 @@ describe('persistence', () => {
   it('should insert with promise', async () => {
     await db.insert({ name: 'Maggie' });
     const item = await db.find({ name: 'Maggie' });
-    expect(item.name).toEqual('Maggie');
+    expect(item[0].name).toEqual('Maggie');
+  });
+
+  it('should insert with callback', async () => {
+    return new Promise((resolve) => {
+      db.insert({ name: 'Maggie' }, (err, res) => {
+        expect(res.name).toEqual('Maggie')
+        resolve()
+      });
+    })
   });
 });
