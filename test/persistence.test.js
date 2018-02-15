d5973e021bfabbaadf23b3a42342441982fa5a2b
antoniosilva <antoniopresto@gmail.com>
2018-02-15 18:32:11 -0200

    fix #21

--- test/persistence.test.js
+++ test/persistence.test.js
@@ -9,13 +9,13 @@ it('update with promise', async () => {
 
   const items = await db.find({})
 
-  const maggie1 = await db.findOne({name: 'Maggie'})
-  const bob1 = await db.findOne({name: 'Bob'})
+  const maggie1 = await db.findOne({ name: 'Maggie' })
+  const bob1 = await db.findOne({ name: 'Bob' })
 
-  await db.update({ name: {$in: ['Maggie', 'Bob']} }, { $set: { age: 1 }}, {multi: true})
+  await db.update({ name: { $in: ['Maggie', 'Bob'] } }, { $set: { age: 1 } }, { multi: true })
 
-  const maggie2 = await db.findOne({name: 'Maggie'})
-  const bob2 = await db.findOne({name: 'Bob'})
+  const maggie2 = await db.findOne({ name: 'Maggie' })
+  const bob2 = await db.findOne({ name: 'Bob' })
 
   expect(items0).toHaveLength(0)
   expect(items).toHaveLength(2)
@@ -24,7 +24,6 @@ it('update with promise', async () => {
   expect(bob2.age).toEqual(1)
   expect(maggie2.age).toEqual(1)
 })
-
 
 it('update with callback', async done => {
   const db = await getDb()
@@ -53,7 +52,6 @@ it('update with callback', async done => {
   })
 })
 
-
 it('remove with callback', async done => {
   const db = await getDb()
   const items0 = await db.find({})
@@ -63,7 +61,7 @@ it('remove with callback', async done => {
 
   const items = await db.find({})
 
-  db.remove({ name: { $in: ['Bob'] } }, {multi: true}, async function(err, res) {
+  db.remove({ name: { $in: ['Bob'] } }, { multi: true }, async function(err, res) {
     const bob2 = await db.findOne({ name: 'Bob' })
 
     expect(res).toEqual(1)
@@ -72,4 +70,37 @@ it('remove with callback', async done => {
     expect(bob2).toBeNull()
     done()
   })
+})
+
+it('resolve remove nonexistent', async done => {
+  const db = await getDb()
+  const items0 = await db.find({})
+
+  await db.insert({ name: 'Maggie' })
+  await db.insert({ name: 'Bob' })
+
+  const items = await db.find({})
+
+  db.remove({ name: 'nonexistent' }, { multi: true }, async function(err, res) {
+    const nonexistent = await db.findOne({ name: 'nonexistent' })
+
+    expect(res).toEqual(0)
+    expect(items0).toHaveLength(0)
+    expect(items).toHaveLength(2)
+    expect(nonexistent).toBeNull()
+    done()
+  })
+})
+
+it('resolve findOne nonexistent', async () => {
+  const db = await getDb()
+  await db.insert({ name: 'Maggie' })
+  await db.insert({ name: 'Bob' })
+
+  const items = await db.find({ name: 'nonexistent' })
+
+  const item = await db.findOne({ name: 'nonexistent' }, function() {})
+
+  expect(item).toBeNull()
+  expect(items.length).toEqual(0)
 })
