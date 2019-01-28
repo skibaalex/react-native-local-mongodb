5108fd0de95b9adcb2a87eacee5a51971e5971df
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2019-01-28 22:50:24 +0400

    the async methods are typed now (#46)
    * the async methods are typed now
    
    * Update typings.d.ts
    
    The Document type clashes with Dom and browser API
    
    * Update typings.d.ts

--- lib/typings.d.ts
+++ lib/typings.d.ts
@@ -28,7 +28,7 @@ declare module 'react-native-local-mongodb' {
     multi?: boolean;
   }
 
-  export interface Document {
+  export interface MongoDocument {
     [key: string]: any;
   }
 
@@ -37,54 +37,52 @@ declare module 'react-native-local-mongodb' {
     exec(cb: Callback<T>): void;
     skip(value: number): Cursor<T>;
     limit(value: number): Cursor<T>;
-    sort(doc: Document): Cursor<T>;
+    sort(doc: MongoDocument): Cursor<T>;
   }
 
-  export type Query = any;
+  export type Query = object;
   export type Projection = any;
   export type Callback<T = void> = (err: Error | null, value: T) => void;
-  export type InsertCallback = (err: Error | null, doc: Document) => void;
+  export type InsertCallback = (err: Error | null, doc: MongoDocument) => void;
   export type CountCallback = (err: Error | null, count: number) => void;
-  export type FindCallback = (err: Error | null, docs: Document[]) => void;
-  export type FindOneCallback = (err: Error | null, doc: Document) => void;
+  export type FindCallback = (err: Error | null, docs: MongoDocument[]) => void;
+  export type FindOneCallback = (err: Error | null, doc: MongoDocument) => void;
   export type UpdateCallback = (
     err: Error | null,
     numAffected: number,
-    affectedDocuments: Document | Document[] | null,
+    affectedDocuments: MongoDocument | MongoDocument[] | null,
     upsert: boolean,
   ) => void;
   export type RemoveCallback = (err: Error | null, numAffected: number) => void;
 
-  class Datastore {
+  export default class Datastore {
     constructor(options?: Options);
     public loadDatabase(): void;
     public getAllData(): any[];
     public resetIndexes(newData: any): void;
     public ensureIndex(options: IndexOptions, callback?: Callback): void;
     public removeIndex(fieldName: string, callback?: Callback): void;
-    public addToIndexes(doc: Document): void;
-    public removeFromIndexes(doc: Document): void;
-    public updateIndexes(oldDoc: Document, newDoc: Document): void;
+    public addToIndexes(doc: MongoDocument): void;
+    public removeFromIndexes(doc: MongoDocument): void;
+    public updateIndexes(oldDoc: MongoDocument, newDoc: MongoDocument): void;
     public getCandidates(query: Query, dontExpireStaleDocs: boolean, callback?: Callback): void;
-    public insert(newDoc: Document, cb: InsertCallback): void;
+    public insert(newDoc: MongoDocument, cb: InsertCallback): void;
     public createNewId(): number;
     public count(query: Query): Cursor<number>;
     public count(query: Query, callback: Callback<number>): void;
-    public find(query: Query): Cursor<Document[]>;
-    public find(query: Query, projection: Projection): Cursor<Document[]>;
-    public find(query: Query, projection: Projection, callback: Callback<Document[]>): void;
-    public findOne(query: Query): Cursor<Document>;
-    public findOne(query: Query, projection: Projection): Cursor<Document>;
-    public findOne(query: Query, projection: Projection, callback: Callback<Document>): void;
-    public update(query: Query, doc: Document, options?: UpdateOptions, callback?: UpdateCallback): void;
+    public find(query: Query): Cursor<MongoDocument[]>;
+    public find(query: Query, projection: Projection): Cursor<MongoDocument[]>;
+    public find(query: Query, projection: Projection, callback: Callback<MongoDocument[]>): void;
+    public findOne(query: Query): Cursor<MongoDocument>;
+    public findOne(query: Query, projection: Projection): Cursor<MongoDocument>;
+    public findOne(query: Query, projection: Projection, callback: Callback<MongoDocument>): void;
+    public update(query: Query, doc: MongoDocument, options?: UpdateOptions, callback?: UpdateCallback): void;
     public remove(query: Query, options?: RemoveOptions, callback?: RemoveCallback): void;
     public loadDatabaseAsync(): Promise<void>;
-    public findAsync(): Promise<Document[]>;
-    public findOneAsync(): Promise<Document>;
-    public insertAsync(): Promise<Document>;
-    public updateAsync(): Promise<Document>;
-    public removeAsync(): Promise<number>;
+    public findAsync(query: Query): Promise<MongoDocument[]>;
+    public findOneAsync(query: Query): Promise<MongoDocument>;
+    public insertAsync(newDoc: MongoDocument): Promise<MongoDocument>;
+    public updateAsync(query: Query, doc: MongoDocument, options?: UpdateOptions): Promise<MongoDocument>;
+    public removeAsync(query: Query, options?: RemoveOptions): Promise<number>;
   }
-
-  export default Datastore;
 }
